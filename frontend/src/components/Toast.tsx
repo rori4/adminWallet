13eafee4fa80b0187275ffefe127c88845a58c52
brock <brocksmedley@gmail.com>
2021-11-07 15:50:13 -0800

    improve toast styling

--- frontend/src/components/Toast.tsx
+++ frontend/src/components/Toast.tsx
@@ -8,8 +8,8 @@ type ToastProps = {
 }
 
 const CustomToast: FunctionComponent<ToastProps> = ({message, show, setShow}) => {
-    return (<Toast show={show} autohide={true} delay={3000} onClose={() => setShow(false)} style={{position: "fixed", bottom: 24, right: 16}}>
-        <Toast.Body>{message}</Toast.Body>
+    return (<Toast bg="info" show={show} autohide={true} delay={3000} onClose={() => setShow(false)} style={{position: "fixed", bottom: 24, right: 16}}>
+        <Toast.Body style={{color: "white", textShadow: "1px 1px #666"}}>{message}</Toast.Body>
     </Toast>)
 }
 
